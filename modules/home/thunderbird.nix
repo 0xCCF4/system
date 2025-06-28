73b576a47f342d3384cc57320cff255cf5d256f3
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-06-28 13:14:23 +0200

    Fix: thunderbird module bugs

--- modules/home/thunderbird.nix
+++ modules/home/thunderbird.nix
@@ -10,22 +10,13 @@ with lib; with builtins;
     ./persistence.nix
   ];
 
-  options.home.mine.thunderbird = with types;
-    {
-      enable = mkOption {
-        type = bool;
-        default = config.home.mine.traits.hasOffice;
-        description = "Enable Thunderbird email client";
-      };
-    };
-
   config = {
-    home.mine.persistence.data.directories = mkIf config.home.mine.thunderbird.enable [
+    home.mine.persistence.data.directories = mkIf config.programs.thunderbird.enable [
       ".thunderbird"
     ];
 
     programs.thunderbird = {
-      enable = mkDefault config.home.mine.thunderbird.enable;
+      enable = mkDefault config.home.mine.traits.hasOffice;
       settings = {
         "general.useragent.override" = "";
         "privacy.donottrackheader.enabled" = true;
