b9679d3415978778b33d31b5682939819e9138eb
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-06-28 16:22:58 +0200

    Add: mkWallpaper nix function

---
+++ lib/binaryWallpapers/default.nix
@@ -0,0 +1,36 @@
+{ pkgs
+, lib
+, ...
+}:
+with lib;
+let
+  mkWallpaper = { name, src_image, src_data, primaryColor, secondaryColor, description }:
+    let
+      baseName = builtins.baseNameOf src_image;
+      outputName = "${baseName}";
+
+      pkg = pkgs.stdenv.mkDerivation {
+        inherit name;
+
+        dontUnpack = true;
+
+        installPhase = ''
+          mkdir -p $out
+          ${pkgs.binary-wallpapers}/bin/binary-wallpapers --image ${src_image} --data ${src_data} --primary-color ${primaryColor} --secondary-color ${secondaryColor} --output $out/${outputName}
+        '';
+
+        passthru = {
+          output = "${pkg}/${outputName}";
+        };
+
+        meta = with lib; {
+          inherit description;
+          homepage = "https://github.com/0xCCF4/system";
+          license = licenses.free;
+          platforms = platforms.all;
+        };
+      };
+    in
+    pkg;
+in
+mkWallpaper
--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,6 @@
-{ noxa, nixpkgs, lib ? nixpkgs.lib, ... }: with lib; with builtins; rec {
+{ noxa, nixpkgs, lib ? nixpkgs.lib, ... }: with lib; with builtins; let 
+  mkWallpaper = import ./mkWallpaper.nix;
+in rec {
   evalMissingOption = config: option: default:
     let
       options = if typeOf option == "string" then strings.splitString "." option else option;
@@ -12,6 +14,9 @@
         with noxa.lib.ansi; (trace "${bold+fgMagenta+underline}Warning${noUnderline}: ${noBold+fgYellow} nixos module option ${fgCyan+optionBeforeMine+italic+optionAfterMine+fgYellow+noItalic} module not found! Did you not import the module in your NixOS settings?" default)
       )
       config);
+
+  mkBinaryWallpaper = {pkgs, lib, ...}@env: { name, src_image, src_data, primaryColor, secondaryColor, description }@params:
+    mkWallpaper env params;
 
   optionalIfExist = path: lists.optional (pathExists path) path;
 }
