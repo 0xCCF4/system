5584ebb95fcd17b81350f5c2b68aa9f35835a884
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-06-28 13:16:59 +0200

    Style: added qcow gitignore

--- hosts/lux.nix
+++ hosts/lux.nix
@@ -1,12 +1,13 @@
 { ... }: {
   imports = [
     ../hardware/netcup.nix
+
+    # Users
     ../users/mx.nix
   ];
 
   config = {
     networking.hostName = "lux";
-
     mine.presets.primary = "server";
   };
 }
