5f1b89201b427f884ce49624881f1e221cc50edf
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-10-28 12:58:53 +0100

    Add: fail2ban increment ban times

--- modules/nixos/fail2ban.nix
+++ modules/nixos/fail2ban.nix
@@ -8,6 +8,12 @@ with lib;
   config = {
     services.fail2ban = {
       enable = mkDefault config.services.sshd.enable;
+      bantime-increment = { # https://nixos.wiki/wiki/Fail2ban
+        enable = true;
+        formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+        maxtime = "168h"; # Do not ban for more than 1 week
+        overalljails = true; # Calculate the bantime based on all the violations
+      };
     };
   };
 }
