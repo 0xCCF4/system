ca060aa386db580ca79e1a34d5b5b01de63e8a37
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-11-14 17:28:52 +0100

    Fix: agenix bug due to remmnant /run/agenix directory

---
+++ modules/nixos/agenix_fix.nix
@@ -0,0 +1,28 @@
+{ inputs
+, pkgs
+, lib
+, config
+, options
+, ...
+}:
+with lib;
+{
+  options.mine.age.fix = with types;  mkOption {
+    type = bool;
+    default = (hasAttr "age" options) && (length (attrNames config.age.secrets)) > 0;
+    description = "Apply agenix bugfix: delete /run/agenix before activation script runs.";
+  };
+
+  config = mkIf config.mine.age.fix {
+    system.activationScripts.agenixBugfix = {
+      text = ''
+        echo "[agenix] deleting ${config.age.secretsDir} to workaround agenix bug"
+        rm -Rf "${config.age.secretsDir}"
+      '';
+      deps = [ "specialfs" ];
+    };
+    system.activationScripts.agenixNewGeneration = {
+      deps = [ "agenixBugfix" ];
+    };
+  };
+}
