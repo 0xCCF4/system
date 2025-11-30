cf7e80de3ff42abb3f9a39c0f5c5acc3ba1d043c
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-11-30 17:18:36 +0100

    add: ssh module

--- modules/noxa/mine/ssh.nix
+++ modules/noxa/mine/ssh.nix
@@ -1,26 +1,22 @@
 { lib, config, ... }: with lib; with builtins;
 {
-  ssh.grants = [ ]
-    ++
-    # From ignis to all other nodes
-    (map
-      (to: ({
-        from.node = "ignis";
-        from.user = "mx";
-        to.node = to;
-        to.user = "mx";
-        #to.sshFingerprint = config.nodes.${to}.configuration.noxa.secrets.options.hostPubkey;
+  nodes =
+    let
+      from = [ "ignis" "solis" ];
+      to = config.nodeNames;
+    in
+    mkMerge (map
+      (from: ({
+        "${from}".ssh.grants = mkMerge (map
+          (to: {
+            "${to}" = {
+              from = "mx";
+              to.node = to;
+              to.user = "mx";
+              options.pty = true;
+            };
+          })
+          (filter (name: name != from) to));
       }))
-      (filter (name: name != "ignis") config.nodeNames))
-    ++
-    # From solis to all other nodes
-    (map
-      (to: ({
-        from.node = "solis";
-        from.user = "mx";
-        to.node = to;
-        to.user = "mx";
-        #to.sshFingerprint = config.nodes.${to}.configuration.noxa.secrets.options.hostPubkey;
-      }))
-      (filter (name: name != "solis") config.nodeNames));
+      from);
 }
