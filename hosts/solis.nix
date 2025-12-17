0e5dc966bb5c729a231ff78546869c96a30d56b5
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-12-17 12:56:16 +0100

    add: solis add local dns server to allow fast installation of new servers

--- hosts/solis.nix
+++ hosts/solis.nix
@@ -8,37 +8,115 @@
     ../external/private/hosts/solis.nix
   ];
 
-  config = {
-    # General settings
-    networking.hostName = "solis";
-    mine.presets.primary = "workstation";
-    networking.hostId = "57c565f7";
+  config =
+    let
+      ifaceExternal = "enp0s20f0u1u2u3";
+      ifaceInternal = "enp0s13f0u3u4u5";
+    in
+    {
+      # General settings
+      networking.hostName = "solis";
+      mine.presets.primary = "workstation";
+      networking.hostId = "57c565f7";
 
-    mine.persistence.enable = true;
+      mine.persistence.enable = true;
 
-    mine.locale.keyboardLayout = "us";
+      mine.locale.keyboardLayout = "us";
 
-    specialisation."de-keyboard".configuration = {
-      mine.locale.keyboardLayout = mkForce "de";
-    };
+      specialisation."de-keyboard".configuration = {
+        mine.locale.keyboardLayout = mkForce "de";
+      };
 
-    # Battery management
-    mine.tlp.enable = true;
+      # Battery management
+      mine.tlp.enable = true;
 
-    home-manager.users.mx = {
-      config = {
-        home.mine.traits.traits = [
-          "development"
-          "office"
-        ];
-        home.mine.slack.enable = true;
-        services.gromit-mpx.enable = false;
+      home-manager.users.mx = {
+        config = {
+          home.mine.traits.traits = [
+            "development"
+            "office"
+          ];
+          home.mine.slack.enable = true;
+          services.gromit-mpx.enable = false;
+        };
+      };
+
+      security.sudo.wheelNeedsPassword = mkIf config.age.rekey.initialRollout false;
+
+      # programs.evolution = {
+      #   enable = true;
+      #   plugins = with pkgs; [
+      #     evolution-ews
+      #   ];
+      # };
+
+      mine.desktop.hyprland.enable = true;
+      mine.desktop.gnome.enable = true;
+
+      services.kea.dhcp4 = {
+        enable = true;
+        settings = {
+          interfaces-config.interfaces = [ ifaceInternal ];
+
+          lease-database = {
+            name = "/var/lib/kea/dhcp4-leases.csv";
+            type = "memfile";
+            persist = true;
+            lfc-interval = 3600;
+          };
+
+          valid-lifetime = 4000;
+          renew-timer = 1000;
+          rebind-timer = 2000;
+
+          subnet4 = [
+            {
+              id = 1;
+              subnet = "10.10.10.0/24";
+              pools = [
+                {
+                  pool = "10.10.10.16 - 10.10.10.128";
+                }
+              ];
+
+              option-data = [
+                {
+                  name = "routers";
+                  data = "10.10.10.1";
+                }
+                {
+                  name = "domain-name-servers";
+                  data = "9.9.9.9";
+                }
+              ];
+            }
+          ];
+        };
+      };
+
+      networking.nat = {
+        enable = true;
+        internalInterfaces = [ ifaceInternal ];
+        externalInterface = ifaceExternal;
+      };
+
+      networking.interfaces = {
+        enp0s13f0u3u4u5 = {
+          # connect to private network
+          useDHCP = false;
+          ipv4.addresses = [
+            {
+              address = "10.10.10.1";
+              prefixLength = 24;
+            }
+          ];
+        };
+      };
+
+      networking.firewall.allowedUDPPorts = [ 67 ]; # DHCP
+
+      boot.kernel.sysctl = {
+        "net.ipv4.conf.all.forwarding" = true;
       };
     };
-
-    security.sudo.wheelNeedsPassword = mkIf config.age.rekey.initialRollout false;
-
-    mine.desktop.hyprland.enable = true;
-    mine.desktop.gnome.enable = true;
-  };
 }
