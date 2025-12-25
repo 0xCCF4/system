9e57db2f2f1753d7293282269a1fe901fd6cca8d
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-12-25 20:32:39 +0100

    add: home wireguard net

--- hosts/ignis.nix
+++ hosts/ignis.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, mine, config, ... }: with lib; {
+{ lib, pkgs, mine, config, noxa, ... }: with lib; {
   imports = [
     ../hardware/lenovoThinkpadL14amd.nix
 
@@ -33,5 +33,41 @@
 
     mine.desktop.hyprland.enable = true;
     mine.desktop.gnome.enable = true;
+
+
+    programs.adb.enable = true;
+    users.users.mx.extraGroups = [ "adbusers" "kvm" ];
+
+
+    # BGL VPN
+    noxa.secrets.def = [
+      {
+        ident = "bgl-keypair";
+        module = "mine.wireguard";
+      }
+      {
+        ident = "bgl-presharedkey";
+        module = "mine.wireguard";
+      }
+    ];
+    networking.wg-quick.interfaces.bgl = {
+      address = [ "192.168.138.205/24" ];
+      privateKeyFile = config.age.secrets.${noxa.lib.secrets.computeIdentifier {
+        ident = "bgl-keypair";
+        module = "mine.wireguard";
+      }}.path;
+      autostart = false;
+      peers = [
+        {
+          publicKey = "hMAUZ1zVQIfpgQJef5mgHb40MC7rUvsAzs0l6j8qVkQ=";
+          presharedKeyFile = config.age.secrets.${noxa.lib.secrets.computeIdentifier {
+            ident = "bgl-presharedkey";
+            module = "mine.wireguard";
+          }}.path;
+          allowedIPs = [ "192.168.138.0/24" ];
+          endpoint = "1pmo4cjrsego920r.myfritz.net:53841"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+        }
+      ];
+    };
   };
 }
