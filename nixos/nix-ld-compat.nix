12dd7afbed37ca06a1f5e6ae3eb194e50f9a6f5b
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2026-05-02 17:01:04 +0200

    add: nix ld compat mode

---
+++ nixos/nix-ld-compat.nix
@@ -0,0 +1,136 @@
+{config,lib,pkgs,...}:
+with lib;
+{
+    config = {
+        programs.nix-ld.enable = config.mine.presets.isWorkstation;
+
+        programs.nix-ld.libraries = with pkgs; [
+            # intellij
+            SDL
+            SDL2
+            SDL2_image
+            SDL2_mixer
+            SDL2_ttf
+            SDL_image
+            SDL_mixer
+            SDL_ttf
+            alsa-lib
+            at-spi2-atk
+            at-spi2-core
+            atk
+            bzip2
+            cairo
+            cups
+            curlWithGnuTls
+            dbus
+            dbus-glib
+            desktop-file-utils
+            e2fsprogs
+            expat
+            flac
+            fontconfig
+            freeglut
+            freetype
+            fribidi
+            fuse
+            fuse3
+            gdk-pixbuf
+            glew110
+            glib
+            gmp
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gstreamer
+            gtk2
+            harfbuzz
+            icu
+            keyutils.lib
+            libGL
+            libGLU
+            libappindicator-gtk2
+            libcaca
+            libcanberra
+            libcap
+            libclang.lib
+            libdbusmenu
+            libdrm
+            libgcrypt
+            libgpg-error
+            libidn
+            libjack2
+            libjpeg
+            libmikmod
+            libogg
+            libpng12
+            libpulseaudio
+            librsvg
+            libsamplerate
+            libthai
+            libtheora
+            libtiff
+            libudev0-shim
+            libusb1
+            libuuid
+            libvdpau
+            libvorbis
+            libvpx
+            libxcrypt-legacy
+            libxkbcommon
+            libxml2
+            mesa
+            nspr
+            nss
+            openssl
+            p11-kit
+            pango
+            pixman
+            python3
+            speex
+            stdenv.cc.cc
+            tbb
+            udev
+            vulkan-loader
+            wayland
+            libICE
+            libSM
+            libX11
+            libXScrnSaver
+            libXcomposite
+            libXcursor
+            libXdamage
+            libXext
+            libXfixes
+            libXft
+            libXi
+            libXinerama
+            libXmu
+            libXrandr
+            libXrender
+            libXt
+            libXtst
+            libXxf86vm
+            libpciaccess
+            libxcb
+            xcbutil
+            xcbutilimage
+            xcbutilkeysyms
+            xcbutilrenderutil
+            xcbutilwm
+            xkeyboardconfig
+            xz
+            zlib
+
+            # vivado
+            ncurses5
+            zlib libuuid
+            bash coreutils zlib stdenv.cc.cc
+            libXext libX11 libXrender libXtst
+            libXi libXft libxcb libxcb
+            freetype fontconfig glib gtk2 gtk3
+            graphviz gcc unzip nettools
+
+            # others
+            ffmpeg.dev
+        ];
+    };
+}
